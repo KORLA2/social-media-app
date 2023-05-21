7a3bab46ce68bd0890762d66d7d8a66278d4173b
KORLA GOUTHAM <96729391+KORLA2@users.noreply.github.com>
2023-05-21 14:44:25 +0000

    flex-between-reused

---
+++ redux/src/components/FlexBetween.js
@@ -0,0 +1,7 @@
+import {Box} from '@mui/material'
+import {styled} from '@mui/system'
+export let FlexBetween =styled(Box)({
+    display:'flex',
+    justifyContent:'space-between',
+    alignItems:'center'
+})
